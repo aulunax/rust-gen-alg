14bf6b144096d63d07bd855df9460cf7dca67351
aulunax <kamilony@gmail.com>
2025-04-18 20:44:46 +0200

    small tests improvement

--- src/genalg.rs
+++ src/genalg.rs
@@ -1,3 +1,4 @@
+use core::num;
 use std::error::Error;
 
 use crate::individual::genetic::Genetic;
@@ -9,11 +10,19 @@ pub struct GenAlg<T: Genetic + Clone> {
 
 impl<T: Genetic + Clone> GenAlg<T> {
     pub fn run_genetic_algorithm(
-        &self,
+        &mut self,
         num_of_generations: usize,
-    ) -> Result<&Vec<T>, Box<dyn Error>> {
+    ) -> Result<Vec<T>, Box<dyn Error>> {
 
-        Ok(&self.current_population)
+        for _ in 0..num_of_generations {
+            //
+            // Actual code goes here
+            //
+
+            self.current_generation += 1;
+        }
+
+        Ok(self.current_population.clone())
     }
 
     pub fn new(
@@ -31,12 +40,15 @@ impl<T: Genetic + Clone> GenAlg<T> {
     }
 }
 
+
 #[cfg(test)]
 mod tests {
     use super::*;
     use rand::Rng;
 
-    #[derive(Clone)]
+    const MAX_RAND: i32 = 1000;
+
+    #[derive(Clone, Debug, PartialEq)]
     struct DummyGenetic {
         value: i32,
     }
@@ -44,7 +56,7 @@ mod tests {
     impl Genetic for DummyGenetic {
         fn generate() -> Self {
             let mut rng = rand::rng();
-            let random_value = rng.random_range(0..100);
+            let random_value = rng.random_range(0..MAX_RAND);
 
             DummyGenetic { value: random_value }
         }
@@ -58,14 +70,16 @@ mod tests {
 
     #[test]
     fn test_gen_alg_creation() {
-        let population_size = 50;
-        
+        let population_size = 20;
+
         let gen_alg = GenAlg::<DummyGenetic>::new(population_size, None);
         let gen_vec = &gen_alg.current_population;
 
+        println!("{:?}", gen_vec);
+
         assert_eq!(gen_alg.current_population.len(), population_size);
         assert!(
-            gen_vec.iter().all(|individual| (0..100).contains(&individual.value)),
+            gen_vec.iter().all(|individual| (0..MAX_RAND).contains(&individual.value)),
             "One or more values are out of range!"
         );
         assert!(!(gen_vec.iter().all(|x| x.value == gen_vec[0].value)), 
@@ -75,15 +89,17 @@ mod tests {
 
     #[test]
     fn test_gen_alg_creation_init_pop() {
-        let population_size = 50;
+        let population_size = 20;
         let init_pop: Vec<DummyGenetic> = (0..population_size).map(|_| DummyGenetic::generate()).collect();
 
         let gen_alg = GenAlg::<DummyGenetic>::new(population_size, Some(&init_pop));
         let gen_vec = &gen_alg.current_population;
 
+        println!("{:?}", gen_vec);
+
         assert_eq!(gen_alg.current_population.len(), population_size);
         assert!(
-            gen_vec.iter().all(|individual| (0..100).contains(&individual.value)),
+            gen_vec.iter().all(|individual| (0..MAX_RAND).contains(&individual.value)),
             "One or more values are out of range!"
         );
         assert!(!(gen_vec.iter().all(|x| x.value == gen_vec[0].value)), 
@@ -91,13 +107,26 @@ mod tests {
         );
     }
 
-    // #[test]
-    // fn test_run_genetic_algorithm() {
-    //     let population_size = 5;
-    //     let gen_alg = GenAlg::<DummyGenetic>::new(population_size, None);
-    //     let result = gen_alg.run_genetic_algorithm(10).unwrap();
+    #[test]
+    fn test_run_genetic_algorithm() {
+        let population_size = 20;
+        let num_of_gens = 10;
 
-    //     assert_eq!(result.len(), population_size);
-    // }
+        let mut gen_alg = GenAlg::<DummyGenetic>::new(population_size, None);
+        let result = gen_alg.run_genetic_algorithm(num_of_gens).unwrap();
+        let gen_vec = &gen_alg.current_population;
+
+        println!("{:?}", gen_vec);
+        println!("{:?}", result);
+
+        assert_eq!(result.len(), population_size);
+        assert_eq!(gen_alg.current_generation, num_of_gens);
+        assert!(
+            result.iter().all(|individual| (0..MAX_RAND).contains(&individual.value)),
+            "One or more values are out of range!"
+        );
+        assert_eq!(&result, gen_vec);
+
+    }
 }
 
--- src/individual/individual.rs
+++
@@ -1,6 +0,0 @@
-// use super::genetic::Genetic;
-
-// #[derive(Clone)]
-// pub struct Individual<T: Genetic + Clone> {
-//     pub content: T,
-// }
